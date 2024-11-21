07e91d694fa8a52996cf4566238233acecb92506
Andri <a.halili2004@gmail.com>
2024-11-21 11:08:58 +0100

    Save Cart in LocalStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,11 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart')) || 
+  [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
 
 export function addToCart(productId){
   let matchingItem;
@@ -28,7 +29,13 @@ export function addToCart(productId){
       quantity
     });
   }
+  saveToStorage();
 }
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
 
 export function removeFromCart(productID){
   const newCart = [];
@@ -39,5 +46,5 @@ export function removeFromCart(productID){
   });
 
   cart = newCart;
-
+  saveToStorage();
 }
